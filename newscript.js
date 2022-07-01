dcc5b544a8325425425c4e8833fc9f5c80e3fdfe
Rohitkk432 <74586376+Rohitkk432@users.noreply.github.com>
2022-07-01 14:46:24 +0530

    minor fixes

--- newscript.js
+++ newscript.js
@@ -110,7 +110,7 @@ $(document).ready(function(){
                 items: 3,
                 stagePadding: 70,
             },
-            1250:{
+            1300:{
                 items: 3,
                 stagePadding: 0,
             }
